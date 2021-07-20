f7b0d3ab532f903f25777fa875208507685bcba8
dgonzalezo <dgonzalezo@unal.edu.co>
2021-07-20 15:18:10 -0500

    cuda

---
+++ Cuda/README.md
@@ -0,0 +1,67 @@
+# Practica 1: Compuatación paralela y distribuida UNAL 2021-I
+
+## ¿Que es?
+
+Implementaciones secuencial y paralelizada (mediante `pthread`) del filtro de mediana, usado para filtrar ciertos tipos de ruido, en particular el llamado "de sal y pimienta". Los ejecutables resultantes leen una imagen y crean una segunda imagen, filtrada.
+
+## Dependencias
+
+- Obligatorias
+  * `OpenCV`
+- Opcionales
+  * `make` para compilar facilmente en Linux
+  * `cmake` 
+  * `python` para ejecutar el script que prueba todos los tamaños e usos de hilos
+
+## Para compilar:
+
+### Usando Make
+  ```sh
+  $ make # compila el programa secuencial y el programa con hilos
+  $ make d #compila con simbolos de debugging
+  $ make all #preserva archivos objeto
+  ```
+  
+  Para ejecutar:
+  
+  Algoritmo secuencial con ventana de filtro de 5 pixeles de lado. Lee la imagen en `in/img.jpg` y escribe el resultado en `out/img.jpg`.
+  ```sh
+  $ ./sequential 5 in/img.jpg out/img.jpg
+  ```
+  
+  Algoritmo paralelo con ventana de filtro de 5 pixeles de lado, haciendo uso de 4 hilos `POSIX`. Lee la imagen en `in/img.jpg` y escribe el resultado en `out/img.jpg`.
+  ```sh
+  $ ./paralel 5 4 in/img.jpg out/img.jpg
+  ```
+  
+  Script que transforma todas las imagenes presentes en el directorio `in`, haciendo uso de 1, 2, 4, 8 y 16 hilos, y registrando el tiempo tomado por cada coombinación de parámetros en el archivo `log.txt`.
+  ```sh
+  $ python script_ejecutar_todo.py
+  $ cat log.txt
+  ```
+
+### Usando CMake y Clion
+
+Verifica que en CMakeLists.txt en la linea 3 este correctamente configurado el directorio de OpenCV
+
+```sh
+set(OpenCV_DIR /Users/<your-username>/Path-to-OpenCV)
+```
+
+Si vas a Construir el proyecto Sequencial entonces cambia la linea 9
+
+```sh
+add_executable(Parallel-Computing-2021-1 input.cpp image-effect-<par o seq>.cpp )
+```
+
+Como fue añadido en el .gitgnore que no se guardara la carpeta **cmake-build-debug**, entonces necesitaras configurar el
+proyecto con click derecho en el proyecto (Clion) y "Reload CMake Project", el cual creará la carpet necesaria donde se
+encontrarán los ejecutables despues de hacer build.
+
+Finalmente se ejecutarían reemplazando de los comandos con Make "./paralel" por "cmake-build-debug/Parallel-Computing-2021-1"
+
+por ejemplo: 
+
+```sh
+cmake-build-debug/Parallel-Computing-2021-1 5 4  lena.jpeg lenaExport2.jpg
+```
---
+++ Cuda/image-effect-par.cu
@@ -0,0 +1,170 @@
+#include <iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include "input.hpp"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <opencv2/opencv.hpp>
+#include <typeinfo>
+
+using namespace std;
+using namespace cv;
+using namespace chrono;
+
+Mat img_hsv, img, new_h, new_s, new_v, dst_h, dst_s, dst_v;
+int total_threads;
+int ksize;
+vector<pair<int, int>> delta;
+
+vector<uchar> medianFilterWindow(int i, int j) {
+
+  // Median Calculations for a window of ksize x ksize
+
+  vector<uchar> pixelH(ksize * ksize);
+  vector<uchar> pixelS(ksize * ksize);
+  vector<uchar> pixelV(ksize * ksize);
+
+  for (int k = 0; k < ksize * ksize; ++k) {
+    pixelH[k] = new_h.at<uchar>(i + delta[k].first, j + delta[k].second);
+    pixelS[k] = new_s.at<uchar>(i + delta[k].first, j + delta[k].second);
+    pixelV[k] = new_v.at<uchar>(i + delta[k].first, j + delta[k].second);
+  }
+
+  // Sorting those windows
+  sort(pixelH.begin(), pixelH.end());
+  sort(pixelS.begin(), pixelS.end());
+  sort(pixelV.begin(), pixelV.end());
+
+  // Getting the n/2 element (Median) after sorting for every HSV channel
+  vector<uchar> pixels = {pixelH[(ksize * ksize) / 2],
+                          pixelS[(ksize * ksize) / 2],
+                          pixelV[(ksize * ksize) / 2]};
+  return pixels;
+}
+__global__ void blur_thread(const int *d_image, const int width,
+                            const int height, const int kernel,
+                            const int total_threads, int *d_blur) {
+
+  /* 	int id = blockDim.x * blockIdx.x + threadIdx.x; */
+  /* 	int ir = id * ( height / total_threads ); */
+  /* 	int fr = (id + 1) * ( height / total_threads ); */
+  /*  */
+  /* 	if(id < height){ */
+  /* 		for(int i=0; i<width; i++){ */
+  /* 			for(int j=ir; j<fr; j++){ */
+  /* 				//d_blur[j+i*height] = d_image[j+i*height];; */
+  /* 				d_blur[j+i*height] = cal_intensity(d_image, i,
+   * j, width, height, kernel); */
+  /* 				//d_blur[j*width+i] = cal_intensity(d_image, i,
+   * j, width, height, kernel); */
+  /* 				//d_blur[j+i*height] = d_image[j+i*height]; */
+  /* 				//d_blur[j+i*height] = d_image[j+i*height]; */
+  /* 			} */
+  /* 		} */
+  /* 	} */
+}
+
+void medianFilter(int thread_id) {
+
+  //  Median Filter Function
+
+  int n = new_h.rows / total_threads;
+  int start = n * thread_id - ksize;
+  int end = start + n;
+  start = start < 0 ? 0 : start;
+
+  // Vector of pixels for every median window calculations for HSV channels
+  vector<uchar> pixels = {new_h.at<uchar>(1, 1), new_s.at<uchar>(1, 1),
+                          new_v.at<uchar>(1, 1)};
+
+  for (int i = start; i < end; i++) {
+
+    for (int j = 0; j < new_h.cols - ksize - 1; j++) {
+
+      pixels = medianFilterWindow(i, j);
+
+      dst_h.at<uchar>(i, j) = pixels[0];
+      dst_s.at<uchar>(i, j) = pixels[1];
+      dst_v.at<uchar>(i, j) = pixels[2];
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  // Getting Arguments from execution command
+  checkNumArgs(5, argc, argv[0]);
+
+  // Parsing Arguments
+  ksize = parsePosInt(argv[1]);
+  total_threads = parsePosInt(argv[2]);
+  img = getImg(argv[3]);
+  double matriz_h [img.rows][img.cols];
+  double matriz_s [img.rows][img.cols];
+  double matriz_v [img.rows][img.cols];
+
+  for(int i=0; i<img.rows; i++){
+    for(int j=0; j<img.cols; j++){
+      matriz_h[i][j] = img.at<Vec3b>(i, j)[0];
+      matriz_s[i][j] = img.at<Vec3b>(i, j)[1];
+      matriz_v[i][j] = img.at<Vec3b>(i, j)[2];
+    }
+  }
+
+  // Converting BGR default color mode to HSV in order to apply Median Filter
+  // Correctly Median Filter shouldnt be applied in RGB or BGR color modes
+  // because are not going to get the real Median between all the channels, If
+  // you are using a GrayScale Image is ok, but with Color Images you should do
+  // a HSV conversion
+  cvtColor(img, img_hsv, COLOR_BGR2HSV);
+  vector<Mat> hsvChannels(3);
+  split(img_hsv, hsvChannels);
+
+  // Delta is a vector of the offset for the windowing, and depends of ksize
+  for (int i = 0; i < ksize; i++) {
+    for (int j = 0; j < ksize; j++) {
+      delta.push_back(make_pair(i, j));
+    }
+  }
+
+  new_h = hsvChannels[0];
+  new_s = hsvChannels[1];
+  new_v = hsvChannels[2];
+
+  // Instead of using malloc/free we drawn on C++ Memory Management and
+  // functions like "clone" in OpenCV  library
+  dst_h = new_h.clone();
+  dst_s = new_s.clone();
+  dst_v = new_v.clone();
+
+  // Start timing
+  auto start = high_resolution_clock::now();
+
+// parallel processing
+#pragma omp parallel for
+  for (int i = 0; i < total_threads; i++) {
+
+    medianFilter(i);
+  }
+
+  // End timing
+  auto end = high_resolution_clock::now();
+  duration<double, milli> total_time = (end - start);
+  cout << "time    = " << total_time.count() / 1000 << '\n';
+
+  // After applied MedianFilter for each channel, were merged due to build the
+  // Image Again
+  vector<Mat> channels = {dst_h, dst_s, dst_v};
+  Mat merged, filtered;
+  merge(channels, merged);
+
+  // Changing HSV mode color to BGR again
+  cvtColor(merged, filtered, COLOR_HSV2BGR);
+
+  // Saving Image results
+  /* putImg(filtered, argv[4]); */
+}
