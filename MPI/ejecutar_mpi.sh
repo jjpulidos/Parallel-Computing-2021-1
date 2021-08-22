00387f2825e253cb2aecb77e0c8ec6ec4e5f7ccb
dgonzalezo <dgonzalezo@unal.edu.co>
2021-08-22 18:45:41 -0500

    script to execute

--- MPI/ejecutar_mpi.sh
+++ MPI/ejecutar_mpi.sh
@@ -5,9 +5,9 @@ mpicc -o mpi split-image.cpp `pkg-config --cflags --libs opencv` -lstdc++ -lmpi_
 clear
 for file in $CWD/in/*
 do
-	for((i = 1; i <= 16; i = i + 1))
-	do
-    do  
-      mpirun -np ${i} --hostfile ../../mpi-host ./mpi $(basename $file)
+    for((i = 1; i <= 16; i = i + 1))
+    do
+      echo  "numero de procesos= ${i}, con imagen=$(basename $file)" >> logfile.txt
+      mpirun -np ${i} --hostfile ../../mpi-host ./mpi in/$(basename $file) >>logfile.txt
     done
 done
